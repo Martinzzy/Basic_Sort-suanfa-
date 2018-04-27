fe542fbc8cce8f6a60b2b4305d41b27fa93d7c57
Martinzzy <32003313+Martinzzy@users.noreply.github.com>
2018-04-27 09:13:14 +0800

    Update selectSort.py

--- selectSort.py
+++ selectSort.py
@@ -1,1 +1,12 @@
+#选择排序：搜索整个列表，找出最小项的位置，如果该位置不是列表的第1个位置，算法就会交换这两个位置的项，然后算法到第二个位置并且重复这个过程。
 
+#通过for循环来进行选择排序
+def selectSort(arr):
+  n = len(arr)
+  for i in range(0,n):
+    maxIndex = i
+    for j in range(i+1,n):
+      if arr[j]<arr[minIndex]:
+        j,minIndex = minIndex,j
+    arr[i],arr[minIndex] = arr[minIndex],arr[i]
+  return arr
